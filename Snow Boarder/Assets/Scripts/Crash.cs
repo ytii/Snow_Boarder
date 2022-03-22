2a66a54ed32e76d343ba177caf949ea8bc8610db
Yeti <derekwaderobert@gmail.com>
2022-03-22 14:04:15 -0400

    yep - left off on fine tuning, added trees

--- Snow Boarder/Assets/Scripts/Crash.cs
+++ Snow Boarder/Assets/Scripts/Crash.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class Crash : MonoBehaviour
 {
     [SerializeField] float deathDelay = 0.5f;
+    [SerializeField] ParticleSystem deathEffect;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground") // if the player collides with the ground do sum
         {
+            deathEffect.Play();
             Invoke("ReloadScene", deathDelay);
         }
     }
--- Snow Boarder/Assets/Scripts/Finish.cs
+++ Snow Boarder/Assets/Scripts/Finish.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class Finish : MonoBehaviour
 {
     [SerializeField] float finishDelay = 2f;
+    [SerializeField] ParticleSystem finishEffect;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player") // if the player collides with the finish line do sum
         {
+            finishEffect.Play();
             Invoke("ReloadScene", finishDelay);
         }
     }
